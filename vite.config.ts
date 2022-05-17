77ed36123da7e504cc1d6c58b7b3b62c6094e188
gustperx <gtgustavo20@gmail.com>
2022-05-16 21:12:28 -0400

    add docker compose file

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //server: {    // <-- this object is added
+  //  port: 8000
+  //},
   plugins: [react()]
 })
